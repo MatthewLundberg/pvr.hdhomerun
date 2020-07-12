ac26aa3216c72a41e3113e50c7c0abc6b8a254d1
Matthew Lundberg <matthew.k.lundberg@gmail.com>
2020-07-11 20:23:08 -0500

    Change copyright dates

--- src/Device.cpp
+++ src/Device.cpp
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2017-2018 Matthew Lundberg <matthew.k.lundberg@gmail.com>
+ *      Copyright (C) 2017-2020 Matthew Lundberg <matthew.k.lundberg@gmail.com>
  *      https://github.com/MatthewLundberg/pvr.hdhomerun
  *
  *  This Program is free software; you can redistribute it and/or modify
